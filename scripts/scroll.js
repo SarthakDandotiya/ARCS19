056eb1ec150abd093c36f76ec56a27f8ce897a26
Sarthak Dandotiya <sarthak.dandotiya23@gmail.com>
2019-02-01 10:41:44 +0530

    Landing page start and menu edit

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -31,49 +31,49 @@ function gt6() {
 	$(".main").moveTo(6);
 }
 
-// // left: 37, up: 38, right: 39, down: 40,
-// // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-// var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
-// function preventDefault(e) {
-// 	e = e || window.event;
-// 	if (e.preventDefault) e.preventDefault();
-// 	e.returnValue = false;
-// }
+function preventDefault(e) {
+	e = e || window.event;
+	if (e.preventDefault) e.preventDefault();
+	e.returnValue = false;
+}
 
-// function preventDefaultForScrollKeys(e) {
-// 	if (keys[e.keyCode]) {
-// 		preventDefault(e);
-// 		return false;
-// 	}
-// }
+function preventDefaultForScrollKeys(e) {
+	if (keys[e.keyCode]) {
+		preventDefault(e);
+		return false;
+	}
+}
 
-// function disableScroll() {
-// 	if (window.addEventListener)
-// 		// older FF
-// 		window.addEventListener("DOMMouseScroll", preventDefault, false);
-// 	window.onwheel = preventDefault; // modern standard
-// 	window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-// 	window.ontouchmove = preventDefault; // mobile
-// 	document.onkeydown = preventDefaultForScrollKeys;
-// }
+function disableScroll() {
+	if (window.addEventListener)
+		// older FF
+		window.addEventListener("DOMMouseScroll", preventDefault, false);
+	window.onwheel = preventDefault; // modern standard
+	window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+	window.ontouchmove = preventDefault; // mobile
+	document.onkeydown = preventDefaultForScrollKeys;
+}
 
-// function enableScroll() {
-// 	if (window.removeEventListener)
-// 		window.removeEventListener("DOMMouseScroll", preventDefault, false);
-// 	window.onmousewheel = document.onmousewheel = null;
-// 	window.onwheel = null;
-// 	window.ontouchmove = null;
-// 	document.onkeydown = null;
-// }
+function enableScroll() {
+	if (window.removeEventListener)
+		window.removeEventListener("DOMMouseScroll", preventDefault, false);
+	window.onmousewheel = document.onmousewheel = null;
+	window.onwheel = null;
+	window.ontouchmove = null;
+	document.onkeydown = null;
+}
 
-// //Fixing & Preventing Scrolling while Open Menu
-// function lockScroll() {
-// 	if ($("body").hasClass("lock-scroll")) {
-// 		$("body").removeClass("lock-scroll");
-// 		enableScroll();
-// 	} else {
-// 		$("body").addClass("lock-scroll");
-// 		disableScroll();
-// 	}
-// }
+//Fixing & Preventing Scrolling while Open Menu
+function lockScroll() {
+	if ($("body").hasClass("lock-scroll")) {
+		$("body").removeClass("lock-scroll");
+		enableScroll();
+	} else {
+		$("body").addClass("lock-scroll");
+		disableScroll();
+	}
+}
