c910c0ab70e56e9a71076ec583959c67105ea51c
Sarthak Dandotiya <sarthak.dandotiya23@gmail.com>
2019-02-03 21:41:48 +0530

    Event Desktop

--- scripts/main.js
+++ scripts/main.js
@@ -145,3 +145,70 @@ function lockScroll() {
 		disableScroll();
 	}
 }
+
+// Carousel
+var showcase = $("#carousel");
+$("#carousel").Cloud9Carousel({
+	// yPos: 42,
+	yPos: 5,
+	xPos: 5,
+	yRadius: 100,
+	xRadius: 500,
+	farScale: 0.4,
+	buttonLeft: $("#buttons > .left"),
+	buttonRight: $("#buttons > .right"),
+	autoPlay: 1,
+	smooth: true,
+	bringToFront: true
+});
+
+// $(function() {
+// 	var showcase = $("#showcase");
+
+// 	showcase.Cloud9Carousel({
+// 		yPos: 42,
+// 		yRadius: 48,
+// 		mirrorOptions: {
+// 			gap: 12,
+// 			height: 0.2
+// 		},
+// 		buttonLeft: $(".nav > .left"),
+// 		buttonRight: $(".nav > .right"),
+// 		autoPlay: true,
+// 		bringToFront: true,
+
+// 		onRendered: showcaseUpdated,
+// 		onLoaded: function() {
+// 			showcase.css("visibility", "visible");
+// 			showcase.css("display", "none");
+// 			showcase.fadeIn(1500);
+// 		}
+// 	});
+
+// 	function showcaseUpdated(showcase) {
+// 		var title = $("#item-title").html(
+// 			$(showcase.nearestItem()).attr("alt")
+// 		);
+
+// 		var c = Math.cos((showcase.floatIndex() % 1) * 2 * Math.PI);
+// 		title.css("opacity", 0.5 + 0.5 * c);
+// 	}
+
+// 	$(".nav > button").click(function(e) {
+// 		var b = $(e.target).addClass("down");
+// 		setTimeout(function() {
+// 			b.removeClass("down");
+// 		}, 80);
+// 	});
+
+// 	$(document).keydown(function(e) {
+// 		switch (e.keyCode) {
+// 			case 37:
+// 				$(".nav > .left").click();
+// 				break;
+
+// 			case 39:
+// 				$(".nav > .right").click();
+// 		}
+// 	});
+// });
