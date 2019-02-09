7b63bb784cb6b5a67aaec8fe3dd5b6691ae4a924
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-02-09 21:31:49 +0530

    Modal Try

--- scripts/main.js
+++ scripts/main.js
@@ -239,3 +239,18 @@ if (screen.width >= 800) {
 // 	alternate: true,
 // 	arrows: true
 // });
+
+// Modal
+// MicroModal.init({
+// 	onShow: modal => console.info(`${modal.id} is shown`), // [1]
+// 	onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+// 	// openTrigger: 'data-custom-open', // [3]
+// 	// closeTrigger: 'data-custom-close', // [4]
+// 	disableScroll: true, // [5]
+// 	disableFocus: false, // [6]
+// 	awaitCloseAnimation: false, // [7]
+// 	debugMode: true // [8]
+// });
+// MicroModal.init();
+
+$("#modal").iziModal();
